abb7f6a3a4984dc2acd6b8ad1f5491441fef6fa7
worichka21q31 <160734161+worichka21q31@users.noreply.github.com>
2025-11-11 11:31:57 +0500

    Update Header.h

--- Majazine/Header.h
+++ Majazine/Header.h
@@ -1,8 +1,12 @@
 #pragma once
-
+inline void Getline(std::string& line);
 void SummonmenuNoLog();
 
 void Start();
+
+bool Login();
+
+inline void Err(int time = 1500);
 
 void CreateStorage();
 
@@ -10,3 +14,5 @@ template<typename ArrType>
 void FillArray(ArrType staticArr[], ArrType dinArr[], int size);
 
 void Delete();
+
+void SumonToLogin();
