7fdb62a4fb717e219a5876e47e5c22121d06f48d
worichka21q31 <160734161+worichka21q31@users.noreply.github.com>
2025-11-11 11:32:10 +0500

    Update Majazine.h

--- Majazine/Majazine.h
+++ Majazine/Majazine.h
@@ -1,18 +1,1 @@
-#pragma once
-inline void Getline(std::string& line);
-void SummonmenuNoLog();
 
-void Start();
-
-bool Login();
-
-inline void Err(int time = 1500);
-
-void CreateStorage();
-
-template<typename ArrType>
-void FillArray(ArrType staticArr[], ArrType dinArr[], int size);
-
-void Delete();
-
-void SumonToLogin();
